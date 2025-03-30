93c5237a230fa9cd2894a1bc11b4c288d35421e1
FlashxArrow <128243482+FlashxArrow@users.noreply.github.com>
2025-03-30 23:54:52 +0300

    Animale adaugate

--- AnimalSounds/Program.cs
+++ AnimalSounds/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AnimalSoundsApp.Types;
 
-namespace AnimalSounds
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<Animal> animals = new List<Animal>
         {
+            new Cow(),
+            new Duck(),
+            new Lion()
+        };
+
+        foreach (var animal in animals)
+        {
+            Console.WriteLine($"{animal.Name} makes sound: {animal.MakeSound()}");
         }
     }
 }
