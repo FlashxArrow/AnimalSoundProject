50bc179f7988919f8e681c2f0c7d6c64b5bdb0e7
FlashxArrow <128243482+FlashxArrow@users.noreply.github.com>
2025-04-17 15:11:00 +0300

    Eliminare fisiere build si executabile, adaugare .gitignore

---
+++ AnimalSounds/Types/AnimalType.cs
@@ -0,0 +1,12 @@
+﻿namespace AnimalSoundsApp.Types
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract string MakeSound();
+    }
+}
